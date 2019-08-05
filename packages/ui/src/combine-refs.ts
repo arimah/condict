c9a5c0f9a094abe764f76906312c8a37d7e461da
Arimah <arimah42@gmail.com>
2019-08-05 08:48:07 +0200

    ui: [broken] Convert various files to TS

---
+++ packages/ui/src/combine-refs.ts
@@ -0,0 +1,26 @@
+import {Ref, RefObject, MutableRefObject} from 'react';
+
+type ValidRef<T> =
+  { bivarianceHack(instance: T | null): void }['bivarianceHack'] |
+  RefObject<T>;
+
+export default <T>(...refs: Ref<T>[]): Ref<T> | null => {
+  const validRefs = refs.filter(Boolean) as ValidRef<T>[];
+
+  switch (validRefs.length) {
+    case 0:
+      return null;
+    case 1:
+      return validRefs[0];
+    default:
+      return elem => {
+        validRefs.forEach(ref => {
+          if (typeof ref === 'function') {
+            ref(elem);
+          } else {
+            (ref as MutableRefObject<T | null>).current = elem;
+          }
+        });
+      };
+  }
+};
