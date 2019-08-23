b4f97b5a021db22983e7147046a7769e3c634174
Arimah <arimah42@gmail.com>
2019-08-23 15:53:58 +0200

    server: Use symbol instead of class for IdOf and InputIdOf
    The symbol is actually fully private, since it isn't exported from the
    type, and different IDs remain stubbornly incompatible. Creating a
    little symbol feels like way less overhead than creating two classes
    that are never used.

--- packages/server/src/model/id-of.ts
+++ packages/server/src/model/id-of.ts
@@ -1,6 +1,4 @@
-class IdBase<T extends string> {
-  private kind: T | null = null;
-}
+const IdKind = Symbol();
 
 // This is a hack to get TypeScript to treat different ID types as distinct.
 // The actual value will be a number (hence the `number` part), but by
@@ -8,12 +6,16 @@ class IdBase<T extends string> {
 // we can get TypeScript to reject invalid ID uses, such as attempting to
 // assign a DefinitionId to a LemmaId.
 // If this were Rust, we would just use a transparent wrapper type, but alas.
-export type IdOf<T extends string> = number & IdBase<T>;
+export type IdOf<T extends string> = number & {
+  [IdKind]: T;
+};
 
 // This type is similar to the above, but actually contains a string rather
 // than a number. As the name suggests, it is used for GraphQL input types,
 // as the GraphQL `ID` type is a string.
-export type InputIdOf<T extends string> = string & IdBase<T>;
+export type InputIdOf<T extends string> = string & {
+  [IdKind]: T;
+};
 
 export function toNumberId<T extends string>(id: InputIdOf<T>): IdOf<T> {
   return +id as IdOf<T>;
