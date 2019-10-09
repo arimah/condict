bf8455a546363ae6aeeabe9b3db2ddd38e74e928
Arimah <arimah42@gmail.com>
2019-10-09 08:08:51 +0200

    ui: Add more standard shortcuts - cut, copy, paste, select all

--- packages/ui/src/command/shortcuts.ts
+++ packages/ui/src/command/shortcuts.ts
@@ -9,6 +9,11 @@ const isWindows =
 
 export const Shortcuts = Object.freeze({
   undo: Shortcut.parse('Primary+Z z'),
-
   redo: Shortcut.parse(isWindows ? 'Primary+Y y' : 'Primary+Shift+Z z'),
+
+  cut: Shortcut.parse('Primary+X x'),
+  copy: Shortcut.parse('Primary+C c'),
+  paste: Shortcut.parse('Primary+V v'),
+
+  selectAll: Shortcut.parse('Primary+A a'),
 });
