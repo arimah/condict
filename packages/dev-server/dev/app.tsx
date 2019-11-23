e19e4cb4349c081ce8f48c994580f9f53d324d98
Arimah <arimah42@gmail.com>
2019-11-23 19:13:16 +0100

    dev-server: Update dependencies
    The new `fix:types` script is highly unfortunate. The
    `@types/styled-components` package has a dependency on
    `@types/react-native`, whose definitions clash with the built-in
    type definitions.
    
    See DefinitelyTyped/DefinitelyTyped#33311 for details and status.
    
    As soon as that issue is closed, the script will be removed.

--- packages/dev-server/dev/app.tsx
+++ packages/dev-server/dev/app.tsx
@@ -13,7 +13,7 @@ const App = () =>
   <>
     <h1>Condict dev server</h1>
     <p>Congratulations! If you see this, it means Condict web server is running correctly.</p>
-    <p>Try editing <code>dev/app.js</code> to see hot reloading in action.</p>
+    <p>Try editing <code>dev/app.tsx</code> to see hot reloading in action.</p>
     <p>This file is served from static content: <img src='/heart.png' alt='Green heart'/></p>
     <GlobalStyles/>
   </>;
