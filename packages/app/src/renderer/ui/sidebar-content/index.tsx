f6c9f938916b8f6e775027c8ce4b10771ed2dce2
Arimah <arimah42@gmail.com>
2021-07-12 10:37:04 +0200

    app: Remove the update button from the sidebar
    I will reimplement this as a more noticeable toast-style notification
    later on.

--- packages/app/src/renderer/ui/sidebar-content/index.tsx
+++ packages/app/src/renderer/ui/sidebar-content/index.tsx
@@ -1,7 +1,5 @@
-import {useState, useEffect} from 'react';
 import SearchIcon from 'mdi-react/MagnifyIcon';
 import SettingsIcon from 'mdi-react/CogIcon';
-import DownloadIcon from 'mdi-react/DownloadIcon';
 import {Localized as T} from '@fluent/react';
 
 import TabList from './tab-list';
@@ -17,15 +15,6 @@ export type Props = {
 const SidebarContent = (props: Props): JSX.Element => {
   const {sidebarContainsFocus} = props;
 
-  // FIXME: remove this temporary testing code
-  const [updateAvailable, setUpdateAvailable] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setUpdateAvailable(true);
-    }, Math.round(2000 + 4000 * Math.random()));
-  }, []);
-
   return <>
     <S.SearchButton command='global:search'>
       <SearchIcon/>
@@ -34,12 +23,6 @@ const SidebarContent = (props: Props): JSX.Element => {
 
     <TabList sidebarContainsFocus={sidebarContainsFocus}/>
 
-    {updateAvailable &&
-      <S.Button command='global:update'>
-        <DownloadIcon/>
-        <span><T id='sidebar-update-button'/></span>
-      </S.Button>
-    }
     <S.Button command='global:settings'>
       <SettingsIcon/>
       <span><T id='sidebar-settings-button'/></span>
