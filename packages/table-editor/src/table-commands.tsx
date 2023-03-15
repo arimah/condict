072c3b5aaeae41b40f523fd5b7f5dc40e7354553
Arimah <arimah42@gmail.com>
2023-03-15 18:26:18 +0100

    table-editor: Simplify useTableCommands
    The useCommandGroup hook is specifically designed to allow uses like
    this, by storing `exec` in a ref internally.

--- packages/table-editor/src/table-commands.tsx
+++ packages/table-editor/src/table-commands.tsx
@@ -1,5 +1,3 @@
-import {useCallback, useRef} from 'react';
-
 import {CommandGroup, CommandSpecMap, useCommandGroup} from '@condict/ui';
 
 import {Table} from './value';
@@ -13,19 +11,14 @@ export type Options<D> = {
   disabled?: boolean;
 };
 
-function useTableCommands<D>(options: Options<D>): CommandGroup {
-  // Store some options in a ref to avoid rebuilding the command map
-  // every time the table changes.
-  const {disabled = false, commands, ...otherOptions} = options;
-  const opts = useRef(otherOptions);
-  opts.current = otherOptions;
-
-  const exec = useCallback((cmd: TableCommandFn<D>) => {
-    const nextValue = cmd(opts.current.value);
-    opts.current.onChange(nextValue);
-  }, []);
-
-  return useCommandGroup({commands, exec, disabled});
+export default function useTableCommands<D>(options: Options<D>): CommandGroup {
+  const {disabled = false, commands, value, onChange} = options;
+  return useCommandGroup({
+    commands,
+    disabled,
+    exec: cmd => {
+      const nextValue = cmd(value);
+      onChange(nextValue);
+    },
+  });
 }
-
-export default useTableCommands;
